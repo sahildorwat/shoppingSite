e97449ef99c8a1f0d21c3411d8d7b54c8fad0e82
ssdorwat <ssdorwat@ncsu.edu>
2018-07-05 17:04:44 -0400

    auth guard added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
   token: string;
+  constructor(private router: Router) {}
+
   signupUser ( email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password).then(
       response => console.log(response)
@@ -15,6 +20,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
               res => {
+                this.router.navigate(['/recipes']);
                 console.log(res);
                 firebase.auth().currentUser.getIdToken().then(
                   token => this.token = token
