e0b2cee31cf64425fdbe3e987e3730fa563b7cac
ssdorwat <ssdorwat@ncsu.edu>
2018-07-04 21:41:32 -0400

    able to get data for edit recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +11,38 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-
-  constructor( private route: ActivatedRoute) { }
+  recipeForm: FormGroup;
+  constructor( private route: ActivatedRoute,
+               private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe( (params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null; });
-
+    this.initForm();
   }
 
+  initForm () {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
+    });
+    console.log(this.recipeForm);
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 }
